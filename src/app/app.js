4d6586476aa051093b6adaffb17bf141299a6c12
skorzany <177423232+skorzany@users.noreply.github.com>
2025-06-17 16:22:52 +0200

    Remove the API key for now

--- src/app/app.js
+++ src/app/app.js
@@ -23,7 +23,7 @@ function clearStatus() {
 
 async function getWeatherData(inputs) {
   // I have removed the api key for now so no bots would steal it...
-  const queryTemplate = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputs.location}/?unitGroup=${inputs.unit}&key=BFAR54V3R8J9JKYC7KE2JT5DR`;
+  const queryTemplate = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputs.location}/?unitGroup=${inputs.unit}&key=#`;
   try {
     const data = await fetch(queryTemplate, { mode: 'cors' });
     if (data.status === 404) {
